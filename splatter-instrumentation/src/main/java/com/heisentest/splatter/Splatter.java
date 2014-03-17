b65184dc9d8496ef300bbdfcfbe824e334be9631
Morrison Cole <admin@morrisoncole.co.uk>
2014-03-17 22:02:54 +0000

    Preliminary control flow analysis with Soot. WIP.

--- splatter-instrumentation/src/main/java/com/heisentest/splatter/Splatter.java
+++ splatter-instrumentation/src/main/java/com/heisentest/splatter/Splatter.java
@@ -2,9 +2,16 @@ package com.heisentest.splatter;
 
 import org.apache.log4j.Logger;
 import org.ow2.asmdex.Opcodes;
+import soot.*;
+import soot.options.Options;
+import soot.toolkits.graph.ExceptionalUnitGraph;
+import soot.toolkits.graph.UnitGraph;
+import soot.util.Chain;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class Splatter {
 
@@ -18,27 +25,113 @@ public class Splatter {
     private static final int ASM_API_LEVEL = Opcodes.ASM4;
     private static String applicationApkPath;
     private static String applicationTestApkPath;
+    private static String androidJars;
 
     /**
-     * Expected args: path to application apk, path to test apk.
+     * Expected args: path to application apk, path to test apk, path to android jars.
      */
     public static void main(String args[]) {
         logger.debug("Starting up Splatter!");
 
-        if (args.length == 2) {
+        if (args.length == 3) {
             applicationApkPath = args[0];
             logger.debug(String.format("Using application APK path '%s'", applicationApkPath));
 
             applicationTestApkPath = args[1];
             logger.debug(String.format("Using application test APK path '%s'", applicationTestApkPath));
+
+            androidJars = args[2];
+            logger.debug(String.format("Using Android JARS path '%s'", androidJars));
         }
+
+        calculateControlFlowGraph(applicationApkPath, applicationTestApkPath);
 
         instrumentApk(applicationApkPath, HEISENTEST_SKELETON_APP_NAMESPACE);
         instrumentApk(applicationTestApkPath, HEISENTEST_SKELETON_APP_TEST_NAMESPACE);
     }
 
-    private static void instrumentApk(String testApkLocation, String appNamespace) {
-        File inputApkFile = new File(testApkLocation);
+    private static void calculateControlFlowGraph(String applicationApkPath, String applicationTestApkPath) {
+        setSootOptions(applicationApkPath, applicationTestApkPath);
+
+        Scene.v().loadNecessaryClasses();
+
+//        SootClass sootClass = Scene.v().loadClassAndSupport("com.heisentest.skeletonandroidapp.MainActivity");
+//        sootClass.setApplicationClass();
+
+        final Chain<SootClass> applicationClasses = Scene.v().getApplicationClasses();
+        for (SootClass sootClass : applicationClasses) {
+            // TODO: Should not be hardcoded.
+            if (sootClass.getName().startsWith("com.heisentest")) {
+                runFlowAnalysis(sootClass);
+            }
+        }
+    }
+
+    private static void setSootOptions(String applicationApkPath, String applicationTestApkPath) {
+        // Prefer APKs over JARs
+        Options.v().set_src_prec(Options.src_prec_apk);
+
+        // Folder containing Android JARs for all API levels
+        Options.v().set_android_jars(androidJars);
+
+        // Not really necessary since we're not outputting anything at the moment
+        Options.v().set_output_format(Options.output_format_dex);
+
+        // Soot uses its own classpath, so we add our APKs to it
+        Options.v().set_prepend_classpath(true);
+        Options.v().set_soot_classpath(String.format("%s", applicationTestApkPath));
+
+        // Verbose output for debugging
+        Options.v().set_verbose(false);
+
+        // If a unavailable class is requested, Soot creates a 'phantom' implementation. Not guaranteed to work properly
+        Options.v().set_allow_phantom_refs(true);
+
+        // Process the test APK
+        Options.v().set_process_dir(Arrays.asList(applicationTestApkPath));
+
+        // TODO: Document
+        Options.v().set_whole_program(true);
+        Options.v().set_validate(true);
+    }
+
+    private static void runFlowAnalysis(SootClass sClass) {
+        System.out.println("***************************************");
+        System.out.println(sClass.getName());
+        System.out.println("***************************************");
+        Iterator methodIt = sClass.getMethods().iterator();
+        while (methodIt.hasNext()) {
+            SootMethod m = (SootMethod)methodIt.next();
+            Body b = m.retrieveActiveBody();
+
+            if (!m.getName().startsWith("test")) {
+                continue;
+            }
+
+            System.out.println("=======================================");
+            System.out.println(m.toString());
+
+            UnitGraph graph = new ExceptionalUnitGraph(b);
+
+            Iterator gIt = graph.iterator();
+            while (gIt.hasNext()) {
+                Unit u = (Unit)gIt.next();
+
+                UnitPrinter up = new NormalUnitPrinter(b);
+                up.setIndent("");
+
+                System.out.println("---------------------------------------");
+                u.toString(up);
+                System.out.println(up.output());
+                System.out.println("---------------------------------------");
+            }
+
+            System.out.println("=======================================");
+        }
+    }
+
+    private static void instrumentApk(String apkLocation, String appNamespace) {
+        File inputApkFile = new File(apkLocation);
         File outputApkFile = new File(inputApkFile.getAbsolutePath().replaceFirst("\\.apk", ".splatter.apk"));
 
         try {
@@ -51,6 +144,6 @@ public class Splatter {
         splatterApkProcessor.process(inputApkFile, outputApkFile);
 
         inputApkFile.delete();
-        outputApkFile.renameTo(new File(testApkLocation));
+        outputApkFile.renameTo(new File(apkLocation));
     }
 }
